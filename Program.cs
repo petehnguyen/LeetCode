91b0cfc17a029a93b74e3d41e0fb749c182b2065
Pete Nguyen <petehnguyen@outlook.com>
2022-03-23 20:51:29 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
